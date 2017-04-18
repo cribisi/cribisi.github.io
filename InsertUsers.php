e84f74cadeb24b9d82738441c84f87c2f1559798
Fernando Vazquez del Mercado <feme3458@colorado.edu>
2017-04-18 00:55:05 -0600

    Update InsertUsers.php

--- InsertUsers.php
+++ InsertUsers.php
@@ -1,5 +1,5 @@
 <?php
-// Lucas Sward 
+// Lucas Sward & Fernando Mercado
 // Hound Dogs
 function pg_connection_string_from_database_url() {
 	return "dbname=dff2a27c9i26ki host=ec2-23-23-227-188.compute-1.amazonaws.com port=5432 user=vdlgvifazdpdaj password=1c07055de63f0532e4e4015d9b6b062821708286d27396e7262a40910194394c sslmode=require"; # <- you may want to add sslmode=require there too
@@ -14,28 +14,40 @@ if (!pg_con) {
 $Username = $_REQUEST['username'];
 $Password = $_REQUEST['password'];
 $Reppass = $_REQUEST['reppassword'];
-if($Password == $Reppass)
+
+if($Username == "" || $Password == "" || $Reppass == "")
 {
-	$sql = "INSERT INTO users (username,password) VALUES	
-		('$Username','$Password');";
-		if(pg_query($pg_conn, $sql))
-		{
-			echo "You have successfully signed up!";
-			echo "Test";
-			header("Location: FrontPage.html");
-			exit();
-		}
-		else
-		{
-			echo "Error: Could not execute $sql ."; 
-			pg_error($pg_conn);
-		}
-		include 'NewUserLogin.php';
-	pg_close($pg_conn);
+	echo "Please Insert a Username and Password";	
 }
-else 
+
+else
 {
-	echo "Your passwords do not match, try again";
-	echo "bad test";
+	if(strlen($Username) < 4 || strlen($Username) > 20 || strlen($Password) < 4 || strlen($Password) > 20){
+		echo "Submit a Username and Password between 4 to 20 characters ";
+	}
+
+	else if($Password == $Reppass)
+	{
+		$sql = "INSERT INTO users (username,password) VALUES	
+			('$Username','$Password');";
+			if(pg_query($pg_conn, $sql))
+			{
+				echo "You have successfully signed up!";
+				header("Location: FrontPage.html");
+				exit();
+			}
+			else
+			{
+				echo "Error: Could not execute $sql ."; 
+				pg_error($pg_conn);
+			}
+			include 'NewUserLogin.php';
+		pg_close($pg_conn);
+	}
+	else 
+	{
+		echo "Your passwords do not match, try again";
+	}
 }
+
 ?>
