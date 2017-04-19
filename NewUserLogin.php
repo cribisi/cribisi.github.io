7ddf4aa0cd93ee5e1e9fbb13a50c53f394a2ec4a
Xi Gao <xiga6451@colorado.edu>
2017-04-19 17:49:37 -0600

    Update NewUserLogin.php
    Changed layout of input fields

--- NewUserLogin.php
+++ NewUserLogin.php
@@ -13,9 +13,36 @@
 });
 </script>
 <style>
+input[type=text], select {
+    width: 100%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-sizing: border-box;
+}
+input[type=password], select {
+	width: 100%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-sizing: border-box;
+}
+input[type=submit] {
+    width: 100%;
+    background-color: #53c687;
+    color: white;
+    padding: 14px 20px;
+    margin: 8px 0;
+    border: none;
+    border-radius: 4px;
+}
 h1{
-	padding: 70px 0;
-	text-align: center;
+    padding: 70px 0;
+    text-align: center;
 }
 div {
     border-radius: 5px;
