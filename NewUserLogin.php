31bdfef0fab8f6b62ae15e76bbf449358954aeb3
Xi Gao <xiga6451@colorado.edu>
2017-04-20 12:13:42 -0600

    Update NewUserLogin.php
    Added color to words

--- NewUserLogin.php
+++ NewUserLogin.php
@@ -56,6 +56,7 @@ div {
     border-radius: 5px;
     background-color: none;
     font-family: 'Overlock SC';
+    color: #006622;
 }
 body {
 	background-image: url("https://www.ecotourismgc.com/wp-content/uploads/2015/09/background.jpg");
